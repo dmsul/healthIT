a5441549e2aecc0b7b7175366c831991f2a74009
Daniel Sullivan <sullydm@gmail.com>
2015-05-05 16:29:12 -0400

    Add loop over adoption status to analysis of system FE's

--- driver_mtg_150507.sh
+++ driver_mtg_150507.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 # `payment_outliers`, list of really bad predictions
 xstata do analysis/compare_ourIT_to_CMS.do
-xstata do analysis/summ_cms_ehr_data.do
-xstata do analysis/system_adoption.do 
+# Compare residuals after FE's (like before), also loop over adoption status
 xstata do analysis/systems_variance.do
+
+# xstata do analysis/summ_cms_ehr_data.do
+# xstata do analysis/system_adoption.do 
